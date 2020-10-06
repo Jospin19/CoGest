a9b178b74d8845d96b20a57a5901ed7313585d9d
Jospin19 <nssoekeme@gmail.com>
2020-10-06 13:48:28 +0100

    Ajout des fichiers du projet dans le dépot Git

---
+++ LoginPage/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginPage.ViewModels
+{
+    public class LoginPageViewModel
+    {
+        //Constructeur de la classe
+        public LoginPageViewModel()
+        {
+
+        }
+
+    }
+}
---
+++ LoginPage/Views/LoginView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginPage.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Fonction qui va permettre d'appeller une storyboard que nous aurons réalisé
+        /// </summary>
+        /// <param name="nomStoryboard">Paramètre représentant le nom de la storyboard réalisé</param>
+        public void PlayStoryboard(string nomStoryboard)
+        {
+            Storyboard Animation = this.FindResource(nomStoryboard) as Storyboard;
+
+            Animation.Begin();
+        }
+
+        private void BtnCallLogin_Click(object sender, RoutedEventArgs e)
+        {
+            PlayStoryboard("ShowLogin");
+        }
+
+
+        private void BtnCallSignUp_Click(object sender, RoutedEventArgs e)
+        {
+            PlayStoryboard("ShowSignUp");
+        }
+    }
+}
